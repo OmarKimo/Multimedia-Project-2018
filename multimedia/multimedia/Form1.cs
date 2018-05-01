1c4260fbd6fc5fbade6d568b0248f24a496c4372
OmarKimo <omarkimo80@gmail.com>
2018-05-01 15:02:26 +0200

    add arithmetic test

--- multimedia/multimedia/Form1.cs
+++ multimedia/multimedia/Form1.cs
@@ -123,10 +123,15 @@ namespace multimedia
 
                 //Huffman.build(allCharsDict);
                 //Huffman.extendhuffman();
-                lzw.Main(allCharsDict.Keys.ToList());
                 //string binarizedChars = Huffman.codeData(EncodedText);
-                IList<int> binarized = lzw.Coding(EncodedText);
-                string binarizedChars = lzw.convertbinary(binarized);
+
+                //lzw.Main(allCharsDict.Keys.ToList());
+                //IList<int> binarized = lzw.Coding(EncodedText);
+                //string binarizedChars = lzw.convertbinary(binarized);
+
+                arthmitc.Main(allCharsDict.Keys.ToList(), allCharsDict.Values.ToList());
+                string binarizedChars = arthmitc.buildbinary(EncodedText, allCharsDict.Values.ToList());
+
                 byte[] bytesFile = GetBytes(binarizedChars);
 
                 FileStream file = new FileStream(fileNameWithPath.Split('.').First() + ".bin", FileMode.Create);
@@ -223,8 +228,10 @@ namespace multimedia
                 br.Close();
                 fr.Close();
 
+                //string DecodedText = Huffman.DecodeData(textToBeUnCompressed,generalChars);
 
-                string DecodedText = Huffman.DecodeData(textToBeUnCompressed,generalChars);
+                arthmitc.Main(allCharsDict.Keys.ToList(), allCharsDict.Values.ToList());
+                string DecodedText = arthmitc.buildstring(textToBeUnCompressed, allCharsDict.Keys.ToList());
 
                 FileStream file = new FileStream(fileNameWithPath.Split('.').First() + "_1.txt", FileMode.Create);
                 StreamWriter DecodedFile = new StreamWriter(file);
